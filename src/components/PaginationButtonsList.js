08ba0b994c69e949ea121401b7585fdb44760a99
Alka Chaudhary <alkawp19@gmail.com>
2022-12-30 16:17:26 +0530

    f1

--- src/components/PaginationButtonsList.js
+++ src/components/PaginationButtonsList.js
@@ -1,8 +1,22 @@
-import React from 'react'
-const PaginationButtonsList = () => {
+import React, { useState } from 'react'
+const PaginationButtonsList = ({handler, numOfButtons = 20}) => {
+    const [active, setActive] = useState(0);
+    const buttonClick = (e) => {
+        handler(+e.target.innerText)
+        setActive(+e.target.innerText-1)
+    }
 
     return (
         <div className="pagination-buttons-list">
+            {Array(numOfButtons).fill(0).map((a, idx) =>
+                <button onClick={buttonClick}
+                key={idx}
+                id={`button-${idx+1}`}
+                className={active === idx ? 'active-btn' : null}
+                >
+                    {idx+1}
+                </button>
+            )}
 
         </div>
     )
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fetchPosts } from '../api/fetchPosts'
+import { Post } from './Post'
+import { PaginationButtonsList } from './PaginationButtonsList'
+
 
 
 const PostList = () => {
+
+    const [data, setData] = useState([])
+    const [page, setPage] = useState(1)
+    const [loading, setLoading] = useState(true)
+    useEffect(()=>{
+        setLoading(true)
+        fetchPosts(page, 5).then(dat =>{
+            setData(dat)
+            setLoading(false)
+        })
+    },[page])
+
+    const handleNextPage = (val) => {
+        setPage(val)
+    }
     
     return (
         <>
-
+            <div className='post-list'>
+                {
+                    loading ?
+                        <div className='loader'>Loading...</div>
+                        : data.map(post => <Post body={post.body} title={post.title} key={post.id}/>)
+                }
+            </div>
+            <PaginationButtonsList handler={handleNextPage}/>
         </>
     )
 }
